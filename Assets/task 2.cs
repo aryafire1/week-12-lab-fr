d71a21eb24e455a8912a35bd477f4ca24077a851
aryafire1 <erthorsen876@gmail.com>
2024-03-28 14:22:48 -0400

    things are working rn hit commit to save

--- Assets/task 2.cs
+++ Assets/task 2.cs
@@ -28,10 +28,34 @@ public class task2 : MonoBehaviour
         "Smithy",
     };
 
+    string[] newNames ={
+        "Mario",
+        "Luigi",
+        "Peach",
+        "Bowser",
+        "Iggy",
+        "Morton",
+        "Lemmy",
+        "Ludwig",
+        "Roy",
+        "Wendy",
+        "Larry",
+        "Rosalina",
+        "Daisy",
+        "Yoshi",
+        "Geno",
+    };
+
+    HashSet<string> nameHash = new HashSet<string>();
+    HashSet<string> dupeHash = new HashSet<string>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //PrintArray();
+        nameHash.Add("test");
+
+        PrintHash();
     }
 
     // Update is called once per frame
@@ -40,5 +64,20 @@ public class task2 : MonoBehaviour
         
     }
 
+    void PrintArray()
+    {
+        foreach(var item in names)
+    {
+        Debug.Log(item.ToString());
+    }
+    }
+
+    void PrintHash()
+    {
+        foreach(var item in nameHash)
+    {
+        Debug.Log(item.ToString());
+    }
+    }
 
 }
