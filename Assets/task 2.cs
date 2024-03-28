f6022863559d57f7c29222dd139c7892449fb4a9
aryafire1 <erthorsen876@gmail.com>
2024-03-28 14:03:04 -0400

    added cs files

---
+++ Assets/task 2.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class task2 : MonoBehaviour
+{
+
+    string[] names ={
+        "Mario",
+        "Luigi",
+        "Peach",
+        "Bowser",
+        "Iggy",
+        "Morton",
+        "Lemmy",
+        "Ludwig",
+        "Roy",
+        "Wendy",
+        "Larry",
+        "Rosalina",
+        "Daisy",
+        "Yoshi",
+        "Geno",
+        "Mallow",
+        "Mack",
+        "Johnny",
+        "Booster",
+        "Smithy",
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
