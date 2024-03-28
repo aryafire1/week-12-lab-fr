92e909357147c368a4d136c9ab719ff2e15a8c1b
BSoto413 <149329491+BSoto413@users.noreply.github.com>
2024-03-28 14:44:19 -0400

    task1

--- Assets/task 1.cs
+++ Assets/task 1.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class task1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<string> nameLibrary = new List<string>()
+       {
+        "Mario",
+        "Luigi",
+        "Peach",
+        "Bowser",
+        "Iggy",
+        "Morton",
+        "Lemmy",
+        "Ludwig",
+        "Roy",
+        "Wendy",
+        "Larry",
+        "Rosalina",
+        "Daisy",
+        "Yoshi",
+        "Geno",
+        "Mallow",
+        "Mack",
+        "Johnny",
+        "Booster",
+        "Smithy",
+    };
+
+    public List<string> lastnamelibrary = new List<string>()
     {
-        
+        "A.",
+        "B.",
+        "C.",
+        "D.",
+        "E.",
+        "F.",
+        "G.",
+        "H.",
+        "I.",
+        "J.",
+        "K.",
+        "L.",
+        "M.",
+        "N.",
+        "O.",
+        "P.",
+        "Q.",
+        "R.",
+        "S.",
+        "T.",
+    };
+
+    private Queue<string> loginQueue = new Queue<string>();
+    private bool isSimulating = false;
+
+    IEnumerator Start()
+    {
+        isSimulating = true;
+        while (isSimulating)
+        {
+            yield return new WaitForSeconds(Random.Range(1f, 3f));
+            AddRandomPlayer();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void AddRandomPlayer()
     {
-        
+        string randomName = nameLibrary[Random.Range(0, nameLibrary.Count)];
+        string randomLastName = lastnamelibrary[Random.Range(0, nameLibrary.Count)];
+
+        loginQueue.Enqueue(randomName);
+        Debug.Log(randomName + randomLastName + " has been added to the Queue.");
+        StartCoroutine(LoginPlayer(randomName));
+    }
+    IEnumerator LoginPlayer(string playerName)
+    {
+        Debug.Log("Logging in" + playerName +  "...");
+        yield return new WaitForSeconds(Random.Range(2f, 5f));
+        Debug.Log(playerName + " has logged in.");
+        loginQueue.Dequeue();
     }
 }
